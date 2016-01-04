fc1def20d652a1d271f270a7f5ee41871f25c4c0
xmnboy <xmnboy@yahoo.com>
2016-01-03 18:55:15 -0800

    various, most important add "use strict" and remove possible arguments leak
    Also, simplify timeStampAbs(); add polyfill for Number.isInteger; remove arguments leak eliminates slice or args; move conditional test of consoleLog option to log() function, where it belongs; protype log.print() function; and tested for proper operation on Windows Phone 8.1 webview (worked).

--- log-webview.js
+++ log-webview.js
@@ -17,6 +17,7 @@
 // AMD support
 // ;paf; can probably eliminate this, since limiting to use in a webview
 (function (root, name, factory) {
+    "use strict" ;
     if( typeof module !== "undefined" ) {
         module.exports = factory() ;
     }
@@ -32,7 +33,7 @@
 // @return {Function} `log` method
 // note _log() is a continuation of AMD parameter list above
 function _log() {
-
+    "use strict" ;
     var log ;                           // the function of interest, defined later
     var ua = navigator.userAgent ;
     var isIEModern = (function() {      // means IE10 or IE11, Edge behaves more like Chrome/Webkit
@@ -53,42 +54,53 @@ function _log() {
             return (function() { return (Date.now()-timeStart) ; }) ;
         }
     }()) ;
-    var timeStampAbs = (function() {
-        return (function() { return Date() ; }) ;
-    }()) ;
+    var timeStampAbs = function() { return Date() ; } ;
+
+
+/*
+ * Polyfill to detect integer number type.
+ * See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger
+ */
+    Number.isInteger = Number.isInteger || function(value) {
+        return typeof value === "number" && isFinite(value) && Math.floor(value) === value ;
+    } ;
 
 
 /*
  * Define the log() function.
- * Call it just like console.log() function.
- * Will create a stack of logs that can be popped for later inspection.
- * Prepends filename:line:column info to log (when available).
- * Prepends date/time info to log (when available).
+ * Call it just like calling console.log().
+ * Creates a stack of log arrays that can be popped for later inspection.
+ * Optionally prepends filename:line:column info to log (when available).
+ * Optionally prepends date/time info to log (relative or absolute).
+ * See log.options for setting various options, which can be changed dynamically.
  */
 
     log = function() {
         var i, err ;
-        var args = arguments ;
-        var sliced = Array.prototype.slice.call(args) ; // convert "arguments" to an array
+        var args = new Array(arguments.length) ;    // to prevent "leaked" arguments
+        for( i=0; i<args.length; ++i ) {            // see https://github.com/petkaantonov/bluebird/wiki/Optimization-killers#32-leaking-arguments
+            args[i] = arguments[i] ;
+        }
 
         if( log.options.lineNumber ) {
             err = new Error() ;                         // add filename:line:column info (does not work with IE10 or IE11)
             if( err.fileName && err.lineNumber ) {      // Firefox
-                sliced.unshift("@" + err.fileName.substr(err.fileName.lastIndexOf("/") + 1) + ":" + err.lineNumber + ":1") ;
+                args.unshift("@" + err.fileName.substr(err.fileName.lastIndexOf("/") + 1) + ":" + err.lineNumber + ":1") ;
             }
             else if( err.stack ) {                      // Chrome/WebKit/Edge
-                sliced.unshift(log.getLineFromStack(err.stack)) ;
+                args.unshift(log.getLineFromStack(err.stack)) ;
             }
         }
         if( log.options.timeStamp ) {
             if( log.options.timeRelative )
-                sliced.unshift(timeStampRel()) ;
+                args.unshift(timeStampRel()) ;
             else
-                sliced.unshift(timeStampAbs()) ;
+                args.unshift(timeStampAbs()) ;
         }
 
-        log.history.push(sliced) ;      // push log arguments for later inspection
-        log.console(sliced) ;           // and print to live console
+        log.history.push(args) ;      // push log arguments for later inspection
+        if( log.options.consoleLog )
+            log.console(args) ;       // and print to live console
     } ;
 
     log.history = [] ;                  // for maintaining a history of all logs
@@ -97,19 +109,22 @@ function _log() {
         webview: "unknown",             // which webview are we inside?
     } ;
 
+
 /*
  * need to add an option to control printing to a redirected console...
  */
     log.options = {                     // default logging options
         timeStamp: true,                // true prepends time of log() to the logged array
-        timeRelative: false,            // true means prepend relative time to the logged array
-        lineNumber: true,               // true prepends filename, line number and column number to log
-        consoleLog: false               // true means send to console.log() after pushing to log.history
+        timeRelative: false,            // true prepends relative timeStamp, else absolute timeStamp
+        lineNumber: true,               // true prepends filename, line number and column number to log array
+        consoleLog: false               // true means send to console.log() after pushing to log.history stack
     } ;
 
 
 /*
  * Simple function to test and show how to use the log() function.
+ * Run log.test() from the console command-line and then type log.history to see complete results.
+ * Modify log.options values at console command-line to see what they do (e.g., log.options.consoleLog = true).
  */
     log.test = function() {
         log("This is a string", 3.14159, {'alpha': 5,'bravo': false}, document.getElementsByTagName("head"), new Date()) ;
@@ -127,68 +142,54 @@ function _log() {
  * - the specified log element if one numeric arg is provided, log.print(3)
  * - the range of log elements if two numeric args are provided, log.print(5,9)
  */
-    log.print = function(start,stop) {      // TO BE WRITTEN...
-//        var i ;
-//
-//        // only send to console if we have a usable console AND the option is enabled
-//        if( window.console && typeof console.log === "function" && log.options.consoleLog ) {
-//
-//            if( isIEModern ) {                              // group arguments for IE10 and IE11
-//                console.group("log.console:") ;
-//            }
-//
-//            if( sliced.length === 1 && typeof sliced[0] === "string" ) {    // if a single string argument
-//                console.log(sliced.toString()) ;
-//            }
-//            else if( isIEModern ) {                         // IE 10/11 need `console.dir` to display objects usefully
-//                for( i=0; i < sliced.length; i++ ) {        // loop through argument array to pick out objects
-//                    if( log.kind(sliced[i]) === "object" )  // plain object
-//                        console.dir(sliced[i]) ;
-//                    else                                    // some other type
-//                        console.log(sliced[i]) ;
-//                }
-//            }
-//            else {                                          // all other modern browsers
-//                console.log(sliced) ;
-//            }
-//
-//            if( isIEModern ) {                              // ungroup arguments for IE10 and IE11
-//                console.groupEnd() ;
-//            }
-//
-//            return true ;
-//        }
-//        else
-//            return false ;
+    log.print = function(start,stop) {
+        if( start === undefined ) {         // no parameters provided, print entire history
+            start = 0 ;
+            stop = log.history.length ;
+        }
+        if( stop === undefined ) {          // only start provided, print single history element
+            stop = start ;
+        }
+        if( !Number.isInteger(start) ) {    // bad start parameter, assume starting with 0th element
+            start = 0 ;
+        }
+        if( !Number.isInteger(stop) ) {     // bad stop parameter, assume stopping with last element
+            stop = log.history.length ;
+        }
+        if( start<0 ) {                     // negative start parameter, start with zeroth element
+            start = 0 ;
+        }
+        if( stop>log.history.length ) {     // too large stop parameter, stop at last element
+            stop = log.history.length ;
+        }
+// TODO: iterate from start thru stop and call log.console() each time
     } ;
 
 
 /*
  * Echo log results to standard console.
  */
-    log.console = function(sliced) {
-        var i ;
-
+    log.console = function(argArray) {
         // only send to console if we have a usable console AND the option is enabled
-        if( window.console && typeof console.log === "function" && log.options.consoleLog ) {
+        if( window.console && typeof console.log === "function" ) {
 
             if( isIEModern ) {                              // group arguments for IE10 and IE11
                 console.group("log.console:") ;
             }
 
-            if( sliced.length === 1 && typeof sliced[0] === "string" ) {    // if a single string argument
-                console.log(sliced.toString()) ;
+            if( argArray.length === 1 && typeof argArray[0] === "string" ) {    // if a single string argument
+                console.log(argArray.toString()) ;
             }
             else if( isIEModern ) {                         // IE 10/11 need `console.dir` to display objects usefully
-                for( i=0; i < sliced.length; i++ ) {        // loop through argument array to pick out objects
-                    if( log.kind(sliced[i]) === "object" )  // plain object
-                        console.dir(sliced[i]) ;
+                for( var i=0; i < argArray.length; i++ ) {  // loop through argument array to pick out objects
+                    if( log.kind(argArray[i]) === "object" ) // plain object
+                        console.dir(argArray[i]) ;
                     else                                    // some other type
-                        console.log(sliced[i]) ;
+                        console.log(argArray[i]) ;
                 }
             }
             else {                                          // all other modern browsers
-                console.log(sliced) ;
+                console.log(argArray) ;
             }
 
             if( isIEModern ) {                              // ungroup arguments for IE10 and IE11
@@ -269,30 +270,9 @@ function _log() {
     } ;
 
 
+/*
+ * Publish the log() function and its respective methods and properties.
+ */
     return log ;
 }
 ));
-
-
-/* for possible use with a TBW log.console() function
- *
-// Tell IE9 to use its built-in console
-// ;paf; this may be unnecessary, given our target is IE10 and higher (Win8 && WinPhone8.1), Android 4.4+ and iOS 7+
-if( Function.prototype.bind && /^object$|^function$/.test(typeof console) && typeof console.log === "object" && typeof window.addEventListener === "function" ) {
-    // Supported console methods
-    ["assert", "clear", "dir", "error", "info", "log", "profile", "profileEnd", "warn"]
-        .forEach(function(method) {
-            console[method] = this.call(console[method], console) ;
-        }, Function.prototype.bind) ;
-
-    // Unsupported console methods, fallback to `log`
-    ["_exception", "count", "debug", "dirxml", "group", "groupCollapsed", "groupEnd", "table", "time", "timeEnd", "timeline", "timelineEnd", "timeStamp", "trace"]
-        .forEach(function(method) {
-            console[method] = console.log ;
-        }) ;
-}
-
-}
-
- *
- */
